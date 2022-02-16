79da39cf762d72c36a90d864662d6ad37ff594e1
Elizabeth Hines <ehines5@cnm.edu>
2022-02-16 12:32:55 -0700

    fixed express bug in app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //express required to create express application
-const express = require("express");
-const app = express;
+const express = require('express');
+const app = express();
 
 //express routes
-const routes = require("./router");
+const routes = require('./router');
 
 //hostname and port to view app
-//const hostname =  "127.0.0.1";
-//const port = 4200;
+const hostname =  "127.0.0.1";
+const port = 4200;
 
-//directory for other frontend files
-app.use(express.static("public"));
+//directory for frontend files
+app.use(express.static("./public"));
 
 //route for application
 app.use(routes);
