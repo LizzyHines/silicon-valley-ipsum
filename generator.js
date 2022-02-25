a219e6c9cd8a5c4631dedbf4f8e959bf8ad69938
Elizabeth Hines <ehines5@cnm.edu>
2022-02-24 22:27:51 -0700

    added quotes and light styling

--- generator.js
+++ generator.js
@@ -7,11 +7,23 @@ const svIpsum = new GenerateNewText();
 function GenerateNewText () {
     this.sentences =
         [
-            " It’s not magic, it’s talent and sweat.",
-            " People like me ensure your packets get delivered unsniffed.",
-            " So what do I do? I make sure that one bad config on one key component doesn't bankrupt the entire fucking company.",
-            " That’s what the fuck I do."
+            "We're making the world a better place... through constructing hierarchies for maximum code reuse and extensibility. ",
+            "Everybody involved with the music industry is either stealing it or sharing it. They're all a bunch of assholes, especially Radiohead. ",
+            "Hooli is about innovative technology that makes a difference, transforming the world as we know it; making the world a better place, through minimal message-oriented transport layers. ",
+            "Oh god, the marketing team is having another bike meeting. Douchebags. ",
+            "Pied wiper, wide diaper. ",
+            "Where have you two been? We're playing the multi-channel router team in five minutes.  ",
+            "I'm a LaVeyan Satanist, with some theistic tendencies. ",
+            "Bit soup: it's like alphabet soup but it's ones and zeros instead of the letters...cause it's binary, you know, binary is just ones and zeros. ",
+            "They always travel in groups of five, these programmers: there's always a tall skinny white guy, short skinny Asian guy, fat guy with a ponytail, some guy with crazy facial hair, and then an East Indian guy. ",
+            "I'm sorry if I scared you, I know I have somewhat ghost-like features. My uncle used to say, \"you look like someone starved a virgin to death\". ",
+            "Are there any water fountains in your offices? I own ten percent of an app that locates all nearby water fountains. I'd be happy to discuss it with you. ",
+            "Richard, if you're not an asshole, it creates this kind of asshole vacuum and that void is filled by other assholes, like Jared. ",
+            "You kind of have a...like a king-ish feeling to you. You're like a Norse hero from Valhalla. ",
+            "They actually tried to diagnose me with a wasting disease because of my slender frame. ",
+
         ];
+
 }
 
 //generating random sentence from array
@@ -24,14 +36,14 @@ GenerateNewText.prototype.getRandomSentence = function() {
 GenerateNewText.prototype.getParagraph = function() {
     let paragraph = "";
     //setting minimum number of words
-    let minimumCharacterLength = 225;
+    let minimumCharacterLength = 325;
     let firstSentence = true;
     while (paragraph.length < minimumCharacterLength) {
         if (firstSentence) {
             paragraph = paragraph.concat(this.getRandomSentence());
             firstSentence = false;
         } else {
-            paragraph = paragraph.concat(" "+ this.getRandomSentence());
+            paragraph = paragraph.concat(" " + this.getRandomSentence());
         }
     }
     return paragraph;
